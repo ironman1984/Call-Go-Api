07ffb04e18556a9b578ad103f30fc814d73ef596
Hung Nguyen <duyhungb5@outlook.com>
2022-12-22 11:21:40 +0700

    commit

--- CallApi/Program.cs
+++ CallApi/Program.cs
@@ -4,13 +4,15 @@ using CallApi.Model;
 using RestSharp;
 
 string baseUrl = "http://192.168.1.150:9000";
-var requestModel = new RequestModel();
-requestModel.Url = "https://browserleaks.com/ssl";
-requestModel.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54";
-requestModel.Proxy = "";
-requestModel.AllowRedirect = false.ToString();
-requestModel.Timeout = 100;
-requestModel.RequestMethod = Method.Get;
+var requestModel = new RequestModel
+{
+    Url = "https://browserleaks.com/ssl",
+    UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54",
+    Proxy = "",
+    AllowRedirect = false.ToString(),
+    Timeout = 100,
+    RequestMethod = Method.Get
+};
 
 Console.WriteLine("Start Call Api");
 ApiHelper api = new ApiHelper(baseUrl);
